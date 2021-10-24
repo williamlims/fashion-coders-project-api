47e54a234536824ec833522779324267ee8b31c9
BononiAline <bononialine@gmail.com>
2021-10-24 19:30:14 -0300

    Classe Categoria e Produto

---
+++ EstoqueFashionWebApi/EstoqueFashionWebApi/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstoqueFashionWebApi
+{
+    public class Categoria
+    {
+        public string descricao { get; set; }
+    }
+}
